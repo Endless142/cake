60b45b99ce0e3a064874f82a8712f9c7802156ea
Endless142 <115073227+Endless142@users.noreply.github.com>
2022-11-22 14:21:09 +0300

    Add files via upload

---
+++ Cake/Class1.cs
@@ -0,0 +1,123 @@
+﻿namespace Cake
+{
+    public  class tort
+    {
+        private DateTime data = new DateTime();
+        
+        private int zakazik;
+        public List<string> zakaziks = new List<string>();
+        public tort()
+        {
+           
+        }
+        public void Zakaz(int vvod )
+        {
+            data = DateTime.Now;
+            switch(vvod)
+            {
+                case 0:
+                    zakaziks.Add("Без глазури");
+                    break;
+                case 1:
+                    zakazik += 500;
+                    zakaziks.Add("круг - 500");
+                    break;
+                
+                case 2:
+                    zakazik += 500;
+                    zakaziks.Add("Ваниль - 500");
+                    break;
+                case 3:
+                    zakazik += 500;
+                    zakaziks.Add("1 корж - 500");
+                    break;
+                case 4:
+                    zakazik += 500;
+                    zakaziks.Add("Белая глазурь - 500");
+                    break;
+                case 5:
+                    zakazik += 500;
+                    zakaziks.Add("Ваш декор(готовый проект) - 500");
+                    break;
+                case 6:
+                    zakazik += 600;
+                    zakaziks.Add("Квадрат - 600");
+                    break;
+                case 7:
+                    zakazik += 600;
+                    zakaziks.Add("Клубничный вкус - 600");
+
+                    break;
+                case 8:
+                    zakazik += 600;
+                    zakaziks.Add("2 коржа - 600");
+                    break;
+                case 9:
+                    zakazik += 600;
+                    zakaziks.Add("Розовая глазурь - 600");
+                    break;
+                case 10:
+                    zakazik += 700;
+                    zakaziks.Add("Прямоугольник - 700");
+                    break;
+                case 11:
+                    zakazik += 700;
+                    zakaziks.Add("3 коржа - 700");
+                    break;
+                case 12:
+                    zakazik += 700;
+                    zakaziks.Add("Наш готовый декор - 700");
+                    break;
+                case 13:
+                    zakazik += 800;
+                    zakaziks.Add("Сердечко - 800");
+                    break;
+                case 14:
+                    zakazik += 1000;
+                    zakaziks.Add("Маленький(8 порций) - 1000");
+                    break;
+                case 15:
+                    zakazik += 1000;
+                    zakaziks.Add("Малина - 1000");
+                    break;
+                case 16:
+                    zakazik += 1000;
+                    zakaziks.Add("Совсместное составление декора - 1000");
+                    break;
+                case 17:
+                    zakazik += 1200;
+                    zakaziks.Add("Средний(10 порций) - 1200");
+                    break;
+                case 18:
+                    zakazik += 1500;
+                    zakaziks.Add("Большой(12 порций) - 1500");
+                    break;
+                case 19:
+                    zakazik += 1500;
+                    zakaziks.Add("4 коржа и больше - 1500");
+                    break;
+                case 20:
+                    File.AppendAllText("D:\\Totiks", "\n");
+                    File.AppendAllText("D:\\Totiks", "Заказ от:" + data);
+                    foreach(string  i in zakaziks)
+                    {
+                        File.AppendAllText("D:\\Totiks",i);
+                    }
+                    File.AppendAllText("D:\\Totiks", "Заказ на сумму:" + zakazik);
+                    Console.WriteLine("Вы сделали заказ, закройте программу ");
+
+                    break;
+
+
+
+
+            }
+            
+            
+        }
+
+        public string name;
+        
+
+    }
+}
---
+++ Cake/Program.cs
@@ -0,0 +1,345 @@
+﻿namespace Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            tort vibor = new tort();
+            Console.WriteLine("Соберите торт на своё усмотрение");
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine("  Форма");
+            Console.WriteLine("  Размер");
+            Console.WriteLine("  Вкус");
+            Console.WriteLine("  Количество коржей");
+            Console.WriteLine("  Глазурь");
+            Console.WriteLine("  Декор");
+            Console.WriteLine("  Конец заказа");
+            int position = 2;
+            ConsoleKeyInfo clavisha = Console.ReadKey();
+            Console.Clear();
+            while (clavisha.Key != ConsoleKey.Escape)
+            {
+                Console.WriteLine("Соберите торт на своё усмотрение");
+                Console.WriteLine("---------------------------------------------------------------");
+                Console.WriteLine("  Форма");
+                Console.WriteLine("  Размер");
+                Console.WriteLine("  Вкус");
+                Console.WriteLine("  Количество коржей");
+                Console.WriteLine("  Глазурь");
+                Console.WriteLine("  Декор");
+                Console.WriteLine("  Конец заказа");
+                Console.WriteLine("Ваш Заказ:");
+                foreach(string i in vibor.zakaziks)
+                {
+                    Console.Write(i + ",");
+                }
+                if (clavisha.Key == ConsoleKey.DownArrow)
+                {
+                    position++;
+                }
+                if (clavisha.Key == ConsoleKey.UpArrow)
+                {
+                    position--;
+                }
+               
+
+                if (position == 2 && clavisha.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Выберите:");
+                    Console.WriteLine("  Круг - 500");
+                    Console.WriteLine("  Квадрат - 600");
+                    Console.WriteLine("  Прямоугольник - 700");
+                    Console.WriteLine("  Сердечко - 800");
+                    position = 2;
+                    clavisha = Console.ReadKey();
+                    while (clavisha.Key != ConsoleKey.Escape)
+                    {
+
+                        
+                        Console.Clear();
+                        Console.WriteLine("Выберите:");
+                        Console.WriteLine("  Круг - 500");
+                        Console.WriteLine("  Квадрат - 600");
+                        Console.WriteLine("  Прямоугольник - 700");
+                        Console.WriteLine("  Сердечко - 800");
+
+
+
+                        if (position == 2 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(1);
+                        }
+                        if (position == 3 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(6);
+
+                        }
+                        if (position == 4 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(10);
+                        }
+                        if (position == 5 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(13);
+                            break;
+
+                        }
+
+                        if (clavisha.Key == ConsoleKey.DownArrow)
+                        {
+                            position++;
+                        }
+                        if (clavisha.Key == ConsoleKey.UpArrow)
+                        {
+                            position--;
+                        }
+                        Console.SetCursorPosition(0, position);
+                        Console.WriteLine("->");
+                        clavisha = Console.ReadKey();
+                       
+
+                    }
+                }
+                if (position == 3 && clavisha.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Выберите");
+                    Console.WriteLine("  Маленкий(8 порций) - 1000");
+                    Console.WriteLine("  Средний(10 порций) - 1200");
+                    Console.WriteLine("  Большой(12 порций) - 1500");
+                    position = 2;
+                    clavisha = Console.ReadKey();
+
+                    while (clavisha.Key != ConsoleKey.Escape)
+
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Выберите");
+                        Console.WriteLine("  Маленкий(8 порций) - 1000");
+                        Console.WriteLine("  Средний(10 порций) - 1200");
+                        Console.WriteLine("  Большой(12 порций) - 1500");
+                        if (clavisha.Key == ConsoleKey.DownArrow)
+                        {
+                            position++;
+                        }
+                        if (clavisha.Key == ConsoleKey.UpArrow)
+                        {
+                            position--;
+                        }
+                        if (position == 2 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(14);
+                        }
+                        if (position == 3 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(17);
+                        }
+                        if (position == 4 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(18);
+
+                        }
+                        Console.SetCursorPosition(0, position);
+                        Console.WriteLine("->");
+                        clavisha = Console.ReadKey();
+                        Console.Clear();
+                    }
+                }
+                if (position == 4 && clavisha.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Выберите:");
+                    Console.WriteLine("  Ваниль - 500");
+                    Console.WriteLine("  Клубничный - 600");
+                    Console.WriteLine("  Малина - 1000");
+                    position = 2;
+                    clavisha = Console.ReadKey();
+                    while (clavisha.Key != ConsoleKey.Escape)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Выберите:");
+                        Console.WriteLine("  Ваниль - 500");
+                        Console.WriteLine("  Клубничный - 600");
+                        Console.WriteLine("  Малина - 1000");
+                        if (clavisha.Key == ConsoleKey.DownArrow)
+                        {
+                            position++;
+                        }
+                        if (clavisha.Key == ConsoleKey.UpArrow)
+                        {
+                            position--;
+                        }
+
+                        if (position == 1 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(2);
+                        }
+                        if (position == 2 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(7);
+                        }
+                        if (position == 3 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(15);
+                        }
+                        Console.SetCursorPosition(0, position);
+                        Console.WriteLine("->");
+                        clavisha = Console.ReadKey();
+                        Console.Clear();
+                    }
+                }
+                if (position == 5 && clavisha.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Выберите:");
+                    Console.WriteLine("  1 корж - 500");
+                    Console.WriteLine("  2 коржа - 600");
+                    Console.WriteLine("  3 коржа- 700");
+                    Console.WriteLine("  4 коржа и больше - 1500");
+                    position = 2;
+                    clavisha = Console.ReadKey();
+
+                    while (clavisha.Key != ConsoleKey.Escape)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Выберите:");
+                        Console.WriteLine("  1 корж - 500");
+                        Console.WriteLine("  2 коржа - 600");
+                        Console.WriteLine("  3 коржа- 700");
+                        Console.WriteLine("  4 коржа и больше - 1500");
+                        if (clavisha.Key == ConsoleKey.DownArrow)
+                        {
+                            position++;
+                        }
+                        if (clavisha.Key == ConsoleKey.UpArrow)
+                        {
+                            position--;
+                        }
+
+
+                        if (position == 1 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(3);
+                        }
+                        if (position == 2 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(8);
+                        }
+                        if (position == 3 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(11);
+                        }
+                        if (position == 4 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(19);
+                        }
+                        Console.SetCursorPosition(0, position);
+                        Console.WriteLine("->");
+                        clavisha = Console.ReadKey();
+                    }
+                    
+                }
+                if (position == 6 && clavisha.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Выберите:");
+                    Console.WriteLine("  Без глазури - 0");
+                    Console.WriteLine("  Белая - 500");
+                    Console.WriteLine("  Розовая - 600");
+                    position = 2;
+                    clavisha = Console.ReadKey();
+
+                    while (clavisha.Key != ConsoleKey.Escape)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Выберите:");
+                        Console.WriteLine("  Без глазури - 0");
+                        Console.WriteLine("  Белая - 500");
+                        Console.WriteLine("  Розовая - 600");
+                        if (clavisha.Key == ConsoleKey.DownArrow)
+                        {
+                            position++;
+                        }
+                        if (clavisha.Key == ConsoleKey.UpArrow)
+                        {
+                            position--;
+                        }
+
+                        if (position == 1 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(0);
+                        }
+                        if (position == 2 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(4);
+                        }
+                        if (position == 3 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(9);
+                        }
+                        Console.SetCursorPosition(0, position);
+                        Console.WriteLine("->");
+                        clavisha = Console.ReadKey();
+                    }
+                   
+                }
+                if (position == 7 && clavisha.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Выберите:");
+                    Console.WriteLine("  Ваш декор(готовый проект) - 500");
+                    Console.WriteLine("  Наш готовый декор - 700");
+                    Console.WriteLine("  Совсместное составление декора - 1000");
+                    position = 2;
+                    clavisha = Console.ReadKey();
+
+                    while (clavisha.Key != ConsoleKey.Escape)
+
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Выберите:");
+                        Console.WriteLine("  Ваш декор(готовый проект) - 500");
+                        Console.WriteLine("  Наш готовый декор - 700");
+                        Console.WriteLine("  Совсместное составление декора - 1000");
+                        if (clavisha.Key == ConsoleKey.DownArrow)
+                        {
+                            position++;
+                        }
+                        if (clavisha.Key == ConsoleKey.UpArrow)
+                        {
+                            position--;
+                        }
+                        if (position == 1 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(5);
+                        }
+                        if (position == 2 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(12);
+                        }
+                        if (position == 3 && clavisha.Key == ConsoleKey.Enter)
+                        {
+                            vibor.Zakaz(16);
+                        }
+                        Console.SetCursorPosition(0, position);
+                        Console.WriteLine("->");
+                        clavisha = Console.ReadKey();
+                    }
+                   
+                }
+                if (position == 8 && clavisha.Key == ConsoleKey.Enter)
+                {
+                   vibor.Zakaz(20);
+                   
+                }
+
+                Console.SetCursorPosition(0,position);
+                Console.WriteLine("->");
+                
+                clavisha = Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
